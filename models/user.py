50e800f3c4f938b2384f8c75f01744d250842647
hanane-HUB <hananee.fallah@gmail.com>
2022-11-26 20:06:54 +0330

    core and models created

---
+++ models/user.py
@@ -0,0 +1,31 @@
+from core.db_manager import Base, session
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column('user_id', Integer(), primary_key=True)
+    # todo: id = db.Column(db.Integer , primary_key=True , autoincrement=True)
+    first_name = Column(String(15))
+    last_name = Column(String(15))
+    phone = Column(String(11))
+    email = Column(String(100))
+    password = Column(String(50))
+
+    def __init__(self, id, fname, lname, phone, email, password):
+        self.id = id
+        self.first_name = fname
+        self.last_name = lname
+        self.phone = phone
+        self.email = email
+        self.password = password
+
+    @staticmethod
+    def add(id, fname, lname, phone, email, password):
+        user = User(id, fname, lname, phone, email, password)
+        session.add(user)
+
+        session.commit()
+
+# Base.metadata.create_all(engine)
+# session.commit()
