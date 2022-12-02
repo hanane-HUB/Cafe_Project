addb335405eb0618b714870abd707ffb213442fc
FaezehShahmirzaei <112942929+FaezehShahmirzaei@users.noreply.github.com>
2022-12-02 12:21:36 +0330

    change id field in tablestructur

--- models/table.py
+++ models/table.py
@@ -1,9 +1,21 @@
-from core.db_manager import Base, session
+from core.db_manager import Base, session, engine
 from sqlalchemy import Column, Integer, String, ARRAY
 
 
 class Table(Base):
     __tablename__ = 'tables'
-    id = Column('table_id', Integer(), primary_key=True)
-    position = Column(ARRAY(Integer))
+    id = Column('table_id', Integer, autoincrement=True , primary_key=True)
+    position = Column(String(50))
+    status = Column(String(50))
+
+# table_list = session.query(Table).all()
+# print(table_list)
+
+
+# Base.metadata.create_all(engine)
+# session.commit()
+
+# table=Table(position='gharb' ,status='Reserved' )
+# session.add(table)
+# session.commit()
 
